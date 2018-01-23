e86c5a9f09fb7c059c3ed7be416bac72995ee233
Anton Zdanov <akumael@gmail.com>
2018-01-23 21:40:01 +0200

    add messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./hero.service";
 import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
     imports: [BrowserModule, FormsModule],
-    providers: [],
+    providers: [HeroService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
